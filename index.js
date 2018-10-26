5ec41fedda3daa63176d1652db718a44841ed585
tsuchida <tsuchida@webclass.jp>
2018-10-26 16:23:12 +0900

    オウム返しbotだけ作成

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
 const server = require("express")();
 const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
+const mongodbURI = process.env.MONGODB_URI; //環境変数からMongoDBのURIを取得
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient //mongodbを利用するためのインスタンス
+
 // -----------------------------------------------------------------------------
 // パラメータ設定
 const line_config = {
@@ -17,7 +22,35 @@ server.listen(process.env.PORT || 3000);
 
 // -----------------------------------------------------------------------------
 // ルーター設定
+const bot = new line.Client(line_config);
 server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+    // 先行してLINE側にステータスコード200でレスポンスする。
     res.sendStatus(200);
-    console.log(req.body);
+
+    // すべてのイベント処理のプロミスを格納する配列。
+    let events_processed = [];
+
+    // イベントオブジェクトを順次処理。
+    req.body.events.forEach((event) => {
+
+        var userID = event.source.userId;
+        console.log(userID);
+
+        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+        if (event.type == "message" && event.message.type == "text"){
+            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+            // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+            events_processed.push(bot.replyMessage(event.replyToken, {
+                type: "text",
+                text: event.message.text
+            }));
+        }
+    });
+
+    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} event(s) processed.`);
+        }
+    );
 });
