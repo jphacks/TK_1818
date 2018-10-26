4cfd193677fa1745b2251fb0471703e293f5d124
hokekyo1210 <hokekyo1210@gmail.com>
2018-10-26 18:42:29 +0900

    add reply system
    add reply system
    
    add reply system
    
    add reply system
    
    add reply system
    
    add reply system
    
    add reply system
    
    add reply system
    
    add reply system
    
    add reply system
    
    add reply system
    
    add reply system
    
    add reply system
    
    add reply system
    
    add reply system

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const line_config = {
     channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
 };
 const mongodbURI = process.env.MONGODB_URI; //環境変数からMongoDBのURIを取得
+const mongodbAddress = mongodbURI.split("//")[1].split(":")[0]; //
+
+console.log("mongodbURI: "+ mongodbURI);
+console.log("mogodbAddress: "+mongodbAddress);
 
 // -----------------------------------------------------------------------------
 // Webサーバー設定
@@ -61,13 +65,55 @@ function eventProcessor(event){
 
     if (event.type == "message" && event.message.type == "text"){
         //イベントタイプがメッセージで、かつ、テキストタイプだった場合の処理
-        promise_ret = messageTextProcessor(event);
+        // promise_ret = messageTextProcessor(event);
     }else if(event.type == "follow"){
         // スタートメッセージを送信する
-        promise_ret = replyStartMessage(events_processed);
+        promise_ret = followProcessor(event);
     }
 
     return promise_ret;
+}
+
+//イベントタイプがフォローの処理
+function followProcessor(event){
+    var promise_ret = sendStage1Message(event);
+    return promise_ret;
+}
+
+// 表示or投稿を聞くときの処理
+function sendStage1Message(event){
+    var promise_ret = null;
+
+    var userID = event.source.userId;
+    var userData = getUserData(userID);
+    if(userData == null){
+        console.log("user data is null!");
+        userData = makeNewUserData(userID); //データベース上にuserが登録されていなければ、登録する
+    }
+    promise_ret = replyStartMessage(event); //yes or noのメッセージを送る
+    return promise_ret;
+}
+
+//DB上に新しいユーザを作成する
+function makeNewUserData(userID){
+    var ret_userData = {'userID': userID, status: 1, showData: "", count: 0};
+    MongoClient.connect(mongodbURI, (error, client) => {
+        var collection;
+
+        const db = client.db(mongodbAddress);
+     
+        // コレクションの取得
+        collection = db.collection('users');
+        collection.insertOne(ret_userData, (error, result) => {
+            console.log("inserted!");
+        });
+    });
+    return ret_userData;
+}
+
+//DBに新しいユーザを登録
+function insertNewUserDataToDB(userData){
+
 }
 
 //イベントタイプがメッセージで、かつ、テキストタイプだった場合のevent処理
@@ -75,17 +121,35 @@ function messageTextProcessor(event){
     var promise_ret = null;
     
     var userID = event.source.userId;
-    var userData = getUserData();
+    var userData = getUserData(userID);
+
+
 }
 
-function getUserData(userID){
+async function getUserData(userID){
+    var ret_userData = null;
+    console.log("get!");
+    ret_userData = await getUserDataFromMongoDB(userID);
+    console.log("done!: "+ret_userData);
+    return ret_userData;
+}
 
+async function getUserDataFromMongoDB(userID){
+    var datab = await MongoClient.connect(mongodbURI);
+    console.log("a: "+datab);
+    var db = datab.db(mongodbAddress);
+    console.log("b: "+db);
+    const MyCollection = db.collection('users');
+    console.log("c: "+MyCollection);
+    const result = await MyCollection.find({'userID': userID}).toArray();
+    console.log("d: "+result);
+    return result;
 }
 
 /*
  * スタートメッセージを送信し、プロミスを返す
  */
-function replyStartMessage(events_processed){
+function replyStartMessage(event){
     var promise = bot.replyMessage(event.replyToken, {
         type: "text",
         text: START_MESSAGE
