e5180942ecfe4e6cb883c3ed020397803b908e64
Mahito Fujino <kerukukku1@gmail.com>
2018-10-28 03:26:05 +0900

    fix appearence

--- test.js
+++ test.js
@@ -12,78 +12,9 @@ var async = require('async');
 const messageTemplate = require('./src/modules/MessageTemplate')
 
 for(word in words) ng_dict[words[word]] = true
-console.log(messageTemplate.QuickReplyMessage.getTemplate(
-        "START_MESSAGE", 
-        [{
-            "type": "text",
-            "label": "投稿"
-        },{
-            "type": "text",
-            "label": "表示"
-        }]
-    )
-)
-const morph = require('./src/modules/morph')
-
-const carousel = new LINEMessage(
-    new MainBuilder()
-        .type("flex")
-        .contents(
-            new ContentsBuilder()
-            .type("bubble")
-            .body(
-                new ContentsBuilder()
-                .type('box')
-                .layout('horizontal')
-                .contents(
-                    new ContentsBuilder()
-                    .type("text")
-                    .text("This is Text message")
-                    .wrap(true)
-                    .build()
-                ).build()
-            ).footer(
-                new ContentsBuilder()
-                .type('box')
-                .layout('horizontal')
-                .contents(
-                    new ContentsBuilder()
-                    .type("button")
-                    .style('primary')
-                    .action({
-                        "type": "uri",
-                        "label": "Go",
-                        "uri": "https://google.com"
-                    }).build()
-                ).build()
-            ).build()
-        ).build()
-).content
-
-console.log(JSON.stringify(carousel))
-const options = {
-    method: 'POST',
-    uri: 'https://labs.goo.ne.jp/api/morph',
-    header : {
-        'Content-Type': 'application/json'
-    },
-    body: {
-        "app_id":"9d396dd8b1f90b46532ad56b3ff48b3cc3b19580f66877e0aacbaa852b4e18ad", 
-        "request_id":"record001", 
-        "sentence":"こんにちは、今日はいいお天気ですね土人さん",
-        "info_filter":"form"
-    },
-    json: true
-}
-
-request(options, (err, response, body) => {
-    sentence = verifier.verifyNGWord(ng_dict, response.body.word_list)
-    console.log(sentence)
-})
-
-const doit = async function(){
-    const res = await morph.morphological("こんにちは、今日はいいおっぱいですね")
-    console.log(res)
-}
-
-doit()
+console.log(JSON.stringify(messageTemplate.FlexThemeMessage.getTemplate({
+    date: new Date(),
+    text: 'text',
+    theme: 'つちだくんがうんち踏んだ',
+    category : 'あるある'
+}).content))
