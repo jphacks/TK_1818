d00630c2702ae20131e5087880bc39eaa266adea
hokekyo1210 <hokekyo1210@gmail.com>
2018-10-28 09:48:26 +0900

    naporitan's local_db

--- index.js
+++ index.js
@@ -265,11 +265,29 @@ function stageWriteOKProcessor(event, userData){
 
 function displayTheme(event, text){
     getDBData(event, 'theme', {category:text}, function(e, c, find){
-        var post = {
-            endDate : '0/0/0/0',
-            summary : 'Tsutida kun',
-            category: text
-        };
+        var post = null;
+        if (text == OGIRI) {
+            //大喜利のテーマ
+            post = {
+                endDate: '2018/11/01 12:15:30',
+                summary: '迷惑メールと知りつつも思わず開封してしまったタイトル(件名)とは？',
+                category: text
+            };
+        } else if (text == TSUKOMI) {
+            //つっこみのテーマ
+            post = {
+                endDate: '2018/10/29 14:11:01',
+                summary: 'お父さん！僕には、お金も仕事もやる気もプライドもありません。こんな僕でよければ、娘さんを僕にください！',
+                category: text
+            };
+        } else if (text == ARU) {
+            //あるある
+            post = {
+                endDate: '2018/11/15 10:02:39',
+                summary: '仕事の時は起きれないのに休みの日だけは早く起きれる。',
+                category: text
+            };
+        }
         for(index in find){
             post = find[index]
             break;
